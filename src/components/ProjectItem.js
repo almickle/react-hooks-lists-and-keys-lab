5a86f49c62ac2410b887c2ff4c6d76a1dba662c8
Michael Dear <michaelcdear@gmail.com>
2022-07-21 21:48:36 -0600

    commit tree 8d260e811ebc4260d8a49a58503b53e0e9e86038

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const spansArray = technologies.map((item, index) => {
+    
+    return <span key={item + name}>{item}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {spansArray}
       </div>
     </div>
   );
